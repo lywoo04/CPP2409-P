4b45bc10914166d91a4faf2879bedb432a4154b8
lywoo04 <yimyeonwoo0819@gmail.com>
2024-11-17 04:50:53 +0900

    Func Renaming, Adding comments

--- src/search_restaurant.cpp
+++ src/search_restaurant.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+//푸드 클래스 정의
 class Food{
 private:
     string name;
@@ -9,13 +10,16 @@ private:
     vector<string> features;
 
 public:
+    //생성자 정의
     Food(const string& name, double price, const vector<string>& features)
         : name(name), price(price), features(features) {}
 
-    string GetfoodName() const {
+    //음식 이름을 반환하는 함수
+    string GetFoodName() const {
          return name ;
     }
 
+    //음식의 정보를 출력하는 함수
     void Print_foodInfor() const {
         cout << "이름: " << name << endl 
              << "가격: " << price << endl
@@ -27,6 +31,7 @@ public:
 
     }
 
+    //원하는 특징을 음식이 가지고 있는지 확인하는 함수
     bool HasFeature(const string& desiredFeature) const {
         for (const auto& foodFeature : features) {
             if (desiredFeature == foodFeature) {
@@ -38,6 +43,7 @@ public:
 
 };
 
+//식당 클래스 정의
 class Restaurant{
 private:
     string name;
@@ -47,23 +53,27 @@ private:
     int location[2];
 
 public:
+    //생성자 정의
     Restaurant(const string& name, const Food& food1, const Food& food2, float rating, int x, int y)
         : name(name), food1(food1), food2(food2), rating(rating) {
             location[0] = x;
             location[1] = y; 
             }
 
+    //식당 이름을 반환하는 함수
     string GetRestauName() const {
         return name ;
     }
 
+    //식당 정보를 출력하는 함수
     void Print_infor() const{
         cout << "식당 이름: " << name << endl 
              << "평점: " << rating << endl 
-             << "대표 메뉴: 1." << food1.GetfoodName() << ", 2." << food2.GetfoodName()
+             << "대표 메뉴: 1." << food1.GetFoodName() << ", 2." << food2.GetFoodName()
              << endl << "위치: " << location[0] << ", " << location[1] << endl << endl;
     }
 
+    //식당의 음식 중, 원하는 음식을 선택하여 설명을 출력하는 함수
     void SelectFood(int foodNum) const {
         if (foodNum == 1) {
             food1.Print_foodInfor();
@@ -74,16 +84,17 @@ public:
         }
     }
 
+    //음식이 원하는 음식인지 확인하고, 맞으면 이름을 출력하는 함수
     bool RecommendFood(const string& desiredFeature) const {
         bool foodFound = false;
         
         if (food1.HasFeature(desiredFeature)) {
-            cout <<food1.GetfoodName() << endl;
+            cout <<food1.GetFoodName() << endl;
             foodFound = true;
         }
         
         if (food2.HasFeature(desiredFeature)) {
-            cout <<food2.GetfoodName() << endl;
+            cout <<food2.GetFoodName() << endl;
             foodFound = true;
         }
         
@@ -92,7 +103,7 @@ public:
 
 };
 
-
+//식당, 요리 정보 입력
 const Food woulmidang_f1("buncha", 1.4, {"Vietnamese", "sour", "noodle"});
 const Food woulmidang_f2("pho", 1.1, {"Vietnamese", "beef", "noodle"});
 const Restaurant woulmidang("woulmidang", woulmidang_f1, woulmidang_f2, 5.0, 0, 0);
@@ -113,14 +124,15 @@ const Food shinsacheon_f1("maratang", 1.3, {"chinese", "spicy", "meat", "vegetab
 const Food shinsacheon_f2("guabaorou", 0.9, {"chinese", "sweet", "sour", "pork"});
 const Restaurant shinsacheon("shinsacheon", shinsacheon_f1, shinsacheon_f2, 4.2, 0, 0);
 
-
+//식당 목록 벡터
 vector<Restaurant> restaurantList = {woulmidang, miss420, speedbanjum, joseon,
                                      shinsacheon };
-
+//특징 목록 벡터
 vector<string> featureList = {"korean", "chinese", "japanese", "vietnamese", "italian",
                               "spicy", "sour"
                              };
 
+//특징 목록을 출력하는 함수
 void DisplayFeatures() {
     cout << "모든 특징 목록:" << endl;
     for (const auto& feature : featureList) {
@@ -129,7 +141,8 @@ void DisplayFeatures() {
     cout << endl;
 }
 
-void SelectRestaurant(int restauNum){
+//원하는 식당의 정보를 출력하는 함수
+void DisplayRestaurant(int restauNum){
     if (restauNum > 0 && restauNum <= restaurantList.size()) {
         restaurantList[restauNum - 1].Print_infor();
     } else {
@@ -137,10 +150,11 @@ void SelectRestaurant(int restauNum){
     }
 }
 
-void DisplayRestaurantNames() {
+//식당의 목록을 출력하는 함수
+void DisplayRestaurantList() {
     int num = 1;
 
-    cout << "가게 목록:" << endl;
+    cout << "식당 목록:" << endl;
     for (const auto& name : restaurantList) {
         cout << num << ". " << name.GetRestauName() << endl;
         num += 1;
@@ -148,8 +162,7 @@ void DisplayRestaurantNames() {
     cout << endl;
 }
 
-
-
+//음식의 정보를 출력하는 함수
 void DisplayFoodInfo(int restauNum, int foodNum) {
     if (foodNum == 1 || foodNum == 2) {
         restaurantList[restauNum - 1].SelectFood(foodNum);
@@ -172,9 +185,9 @@ int main(){
     while(num){
         cout << "----------------------------" << endl;
         cout << "[기능 메뉴]" << endl
-             << "1. 가게 정보" << endl
+             << "1. 식당 정보" << endl
              << "2. 음식 추천받기" << endl
-             << "3. 음식점 추천받기" << endl
+             << "3. 식당 추천받기" << endl
              << "4. 종료" << endl
              << "실행할 기능을 선택하세요: ";
 
@@ -183,14 +196,15 @@ int main(){
 
         switch (menu) {
             case 1:
-                DisplayRestaurantNames();
-                cout << "어떤 음식점의 정보를 원하십니까?" << endl
+                DisplayRestaurantList();
+                
+                cout << "어떤 식당의 정보를 원하십니까?" << endl
                      << "번호를 입력하십시오: ";
                 
                 cin >> restauNum;
                 cout << endl;
 
-                SelectRestaurant(restauNum);
+                DisplayRestaurant(restauNum);
 
                 cout << "음식 정보를 원하십니까?" << endl 
                      << "1: 1번 음식" << endl
@@ -206,7 +220,6 @@ int main(){
                 break;
 
             case 2:
-                cout << "입력가능한 특징 목록"  << endl;
                 DisplayFeatures();
                 cout << "원하는 음식의 특징을 입력하십시오: " ;
                 cin >> desiredFeature;
